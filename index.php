ab293679a666ab2fd670854ea0b42cabbe497f36
MarijaGG <ipa23.m.gutane@vtdt.edu.lv>
2025-01-06 09:48:14 +0200

    input searchs (izzuda ierakstu lists??)

--- index.php
+++ index.php
@@ -11,11 +11,23 @@ $config = require "config.php";
 echo "<h1>Blogs</h1>";
 
 $db = new Database($config["database"]);
-$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);;
+$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
+
+if (isset($_GET["search"]) AND $_GET["search"] != ""){
+  // search logic
+  $posts = $db->query("SELECT * FROM posts WHERE content LIKE '". $_GET["search"] .";'")->fetchAll();
+}
+
 // $comments = $db->query("SELECT * FROM comments")
 // $users = $db->query("SELECT * FROM users WHERE userid = $id")
 
-echo "";
+// post - ja maina db content
+// get - atlasta datus
+
+echo "<form>";
+echo "<input name='search' />";
+echo "<button>Search</button>";
+echo "</form>";
 
 echo "<ul>";
   foreach($posts as $x) {
