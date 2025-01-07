d1cad40ac1c5b7a8b469db7769eb7707340f3174
MarijaGG <ipa23.m.gutane@vtdt.edu.lv>
2025-01-07 11:01:14 +0200

    + categories files

---
+++ categories.php
@@ -0,0 +1,45 @@
+
+<?php
+
+// sodien 06.01, taisiisim ierakstu mekletaaju
+
+require "Database.php";
+$config = require "config.php";
+
+echo "<h1>Categories</h1>";
+
+$db = new Database($config["database"]);
+
+$sql = "SELECT * FROM categories";
+$params = [];
+
+if (isset($_GET["search"]) AND $_GET["search"] != ""){
+  // search logic
+  $search = "%" . $_GET["search"] . "%";
+  $sql .= " WHERE category_name LIKE :search;";
+  $params = ["search" => $search];
+}
+
+$posts = $db->query($sql, $params)->fetchAll();
+// else{ $posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC); }
+
+// $comments = $db->query("SELECT * FROM comments")
+// $users = $db->query("SELECT * FROM users WHERE userid = $id")
+
+// post - ja maina db content
+// get - atlasta datus
+
+echo "<form>";
+echo "<input name='search' />";
+echo "<button>Search</button>";
+echo "</form>";
+
+echo "<ul>";
+  foreach($posts as $x) {
+    echo "<li>" . $x['category_name'] . "</li>"; }
+echo "</ul>";
+
+
+
+
+
--- migrations.sql
+++ migrations.sql
@@ -12,3 +12,13 @@ VALUES
 ("otrais ieraksts");
 
 SELECT * FROM `posts`;
+
+CREATE TABLE categories( 
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+    category_name VARCHAR(25));
+
+INSERT INTO categories(category_name) 
+VALUES 
+("Svētki"), 
+("Mūzika"),
+("Sports");
