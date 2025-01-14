75cf08c0ec814d6b8dc0106c026cb17723d445c1
MarijaGG <ipa23.m.gutane@vtdt.edu.lv>
2025-01-14 10:55:59 +0200

    show features + funkcija redirectam

---
+++ controllers/posts/show.php
@@ -0,0 +1,15 @@
+<?php
+
+if(!isset($_GET["id"]) || $_GET["id"] == ""){
+    redirectIfNotFound();
+}
+
+$sql = "SELECT * FROM posts WHERE id = :id";
+$params = ["id" => $_GET["id"]];
+$x = $db->query($sql, $params)->fetch();
+
+if(!$x) {
+    redirectIfNotFound();
+}
+
+require "views/posts/show.view.php";
