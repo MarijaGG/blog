2bcbf6560295bd33863e28463bf6fbc7c46cea7c
MarijaGG <ipa23.m.gutane@vtdt.edu.lv>
2025-02-05 09:26:56 +0200

    shows fixots

--- views/posts/show.view.php
+++ views/posts/show.view.php
@@ -1,6 +1,8 @@
 <?php require "views/components/header.php"; ?>
 <?php require "views/components/navbar.php"; ?>
 
+<h1> <?= htmlspecialchars($x["content"]) ?> </h1>
+
 <a href="edit?id=<?=$x["id"]?>">Edit</a> <br>
 
 <form method="POST" action="/delete">
